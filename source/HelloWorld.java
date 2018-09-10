7101eb8699f0c2287ca46b3513f967c4fa9340c5
pjunwei95 <junwei35@hotmail.com>
2018-09-10 15:47:52 +0800

    put back import statement

--- source/HelloWorld.java
+++ source/HelloWorld.java
@@ -1,3 +1,10 @@
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 
 
 public class HelloWorld{
