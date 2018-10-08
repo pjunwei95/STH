f9687f4c8ff8a212f6e0671f4a74d412970f2742
pjunwei95 <junwei35@hotmail.com>
2018-10-09 00:21:49 +0800

    JsonResponseHandler: added sortByHighest & comparables

--- app/src/main/java/com/JsonResponseHandler.java
+++ app/src/main/java/com/JsonResponseHandler.java
@@ -2,27 +2,31 @@ package com;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import okhttp3.Response;
+
 
 public class JsonResponseHandler {
 
-//    private String jsonInString = "{\"taxonomy\":[{\"tag\":\"IT\",\"confidence_score\":0.5685330033}," +
-//            " {\"tag\":\"HR\",\"confidence_score\":0.5685330033}],\"code\":200}";
 
-    void run(String jsonInString) {
+    public ConfidenceResponse jsonToConfidenceResponse(String jsonInString) {
 
         ObjectMapper mapper = new ObjectMapper();
+        ConfidenceResponse confidenceResponse = null;
 
         try {
 
             // Convert JSON string to Object
-            ConfidenceResponse confidenceResponse = mapper.readValue(jsonInString, ConfidenceResponse.class);
-            System.out.println(confidenceResponse);
+            confidenceResponse = mapper.readValue(jsonInString, ConfidenceResponse.class);
+            confidenceResponse.sortbyHighest();
+//            System.out.println(confidenceResponse);
 
 //            System.out.println("Testing: ");
 //            System.out.println("Getting value of 2nd tag: " +
@@ -38,12 +42,14 @@ public class JsonResponseHandler {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            return confidenceResponse;
         }
     }
 
     public static void main(String[] args) {
-        JsonResponseHandler jsonResponseHandler = new JsonResponseHandler();
-//        jsonResponseHandler.run();
+//        JsonResponseHandler jsonResponseHandler = new JsonResponseHandler();
+//        jsonResponseHandler.jsonToConfidenceResponse();
     }
 
 }
@@ -51,40 +57,50 @@ class ConfidenceResponse{
     private List<Taxonomy> taxonomy;
     private int code;
 
+        public void sortbyHighest() {
+        Collections.sort(taxonomy, Collections.reverseOrder());
+    }
+
     public List<Taxonomy> getTaxonomy() {
         return taxonomy;
     }
 
-    public void setTaxonomy(List<Taxonomy> taxonomy) {
-        this.taxonomy = taxonomy;
-    }
+//    public void setTaxonomy(List<Taxonomy> taxonomy) {
+//        this.taxonomy = taxonomy;
+//    }
 
     public int getCode() {
         return code;
     }
 
-    public void setCode(int code) {
-        this.code = code;
-    }
+//    public void setCode(int code) {
+//        this.code = code;
+//    }
 }
 
-class Taxonomy{
+class Taxonomy implements Comparable<Taxonomy>{
     private String tag;
     private BigDecimal confidence_score;
+    //Underscore naming due to conventions of the result of ParallelDots API
+
+    @Override
+    public int compareTo(Taxonomy other) {
+        return this.getConfidence_score().compareTo(other.getConfidence_score());
+    }
 
     public String getTag() {
         return tag;
     }
 
-    public void setTag(String tag) {
-        this.tag = tag;
-    }
+//    public void setTag(String tag) {
+//        this.tag = tag;
+//    }
 
     public BigDecimal getConfidence_score() {
         return confidence_score;
     }
 
-    public void setConfidence_score(BigDecimal confidence_score) {
-        this.confidence_score = confidence_score;
-    }
+//    public void setConfidence_score(BigDecimal confidence_score) {
+//        this.confidence_score = confidence_score;
+//    }
 }
